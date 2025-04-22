04f8648a8f1a669d2d123274a4169005360ba3a8
Enrique Domínguez <53445849+kikedose@users.noreply.github.com>
2025-04-22 13:46:24 +0000

    updated telescope keybinds

--- .config/nvim/lua/nconf/lazy/telescope.lua
+++ .config/nvim/lua/nconf/lazy/telescope.lua
@@ -1,28 +1,41 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  tag = "0.1.8",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  module = "telescope",
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.8',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope-ui-select.nvim',
+  },
+  module = 'telescope',
 
   config = function()
-    require('telescope').setup({})
+    require('telescope').setup({
+      extensions = {
+        ['ui-select'] = {
+          require('telescope.themes').get_dropdown(),
+        },
+      },
+    })
+
+    pcall(require('telescope').load_extension, 'ui-select')
 
     local builtin = require('telescope.builtin')
 
-    vim.keymap.set("n", "<leader>fg", builtin.git_files, {})
-    vim.keymap.set("n", "<leader>fr", builtin.live_grep, {})
-    vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-    vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-    vim.keymap.set("n", "<leader>fd", builtin.diagnostics, {})
-    vim.keymap.set("n", "<leader>fh", ":Telescope find_files hidden=true <CR>")
-
-    vim.keymap.set('n', '<leader>pws', function()
-      local word = vim.fn.expand("<cword>")
-      builtin.grep_string({ search = word })
-    end)
-    vim.keymap.set('n', '<leader>pWs', function()
-      local word = vim.fn.expand("<cWORD>")
-      builtin.grep_string({ search = word })
-    end)
-  end
+    vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[F]ind [B]uffers' })
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]ind [F]iles' })
+    vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = '[F]ind [G]it files' })
+    vim.keymap.set('n', '<leader>fr', builtin.oldfiles, { desc = '[F]ind [R]ecent files' })
+    vim.keymap.set('n', '<leader>fs', builtin.live_grep, { desc = '[F]ind [String] in files' })
+    vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[F]ind [D]iagnostics' })
+    vim.keymap.set(
+      'n',
+      '<leader>fh',
+      ':Telescope find_files hidden=true <CR>',
+      { desc = '[F]ind [H]idden files' }
+    )
+    vim.keymap.set('n', '<leader>/', function()
+      builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+        previewer = false,
+      })
+    end, { desc = '[/] Fuzzily search in current buffer' })
+  end,
 }
