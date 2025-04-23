69abdf7b9413981683b5d3c2de8dd8c55f89859a
Enrique Domínguez <53445849+kikedose@users.noreply.github.com>
2025-04-23 22:20:03 +0000

    fixed conflicting remaps

--- .config/nvim/lua/nconf/lazy/lsp.lua
+++ .config/nvim/lua/nconf/lazy/lsp.lua
@@ -27,7 +27,7 @@ return {
         vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
         vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
         vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-        vim.keymap.set('n', '<leader>ff', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+        vim.keymap.set('n', '<leader>gf', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
       end,
     })
 
