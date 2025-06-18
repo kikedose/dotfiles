64e59d5fe3b656a643f19178c0c0a61048c80993
Enrique Domínguez <53445849+kikedose@users.noreply.github.com>
2025-06-18 17:00:59 -0300

    lsp tinkering

--- .config/nvim/lsp/ts_ls.lua
+++ .config/nvim/lsp/ts_ls.lua
@@ -1,14 +1,15 @@
 return {
-  -- init_options = { hostInfo = 'neovim' },
-  -- cmd = { 'typescript-language-server', '--stdio' },
-  -- filetypes = {
-  --   'javascript',
-  --   'javascriptreact',
-  --   'javascript.jsx',
-  --   'typescript',
-  --   'typescriptreact',
-  --   'typescript.tsx',
-  -- },
-  -- root_markers = {'tsconfig.json', 'jsconfig.json', 'package.json', '.git'},
-  -- single_file_support = true,
+  on_attach = on_attach,
+  init_options = { hostInfo = 'neovim' },
+  cmd = { 'typescript-language-server', '--stdio' },
+  filetypes = {
+    'javascript',
+    'javascriptreact',
+    'javascript.jsx',
+    'typescript',
+    'typescriptreact',
+    'typescript.tsx',
+  },
+  root_markers = {'tsconfig.json', 'jsconfig.json', 'package.json', '.git'},
+  single_file_support = true,
 }
