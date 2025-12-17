ee61fecab162fce4deeb52a134b5d09034297c20
Enrique Domínguez <edominguez.se@gmail.com>
2025-12-17 16:22:29 -0300

    updated telescope to v2

--- .config/nvim/lua/config/plugins/treesitter.lua
+++ .config/nvim/lua/config/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
   build = ':TSUpdate',
 
   config = function()
-    require('nvim-treesitter.configs').setup({
+    require('nvim-treesitter').setup({
       ensure_installed = {
         'javascript',
         'typescript',
